5783d6c2c73e79c5d1db8555d7ae281100965da1
KuhnTran <KuhnTran@gmail.com>
2019-09-21 19:40:15 -0700

    Add files via upload

---
+++ testCases/myTest.java
@@ -0,0 +1,16 @@
+public class myTest
+{
+	public static void print(int a, int b, int c, int d)
+	{
+		System.out.println(a + b + c + d);
+	}
+
+	public static void main(String[] args)
+	{
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		int d = 0;
+		print(a,b,c,d);
+	}
+}
